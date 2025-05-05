f8bbf6ab9bcdd873faf6a313d39bbbbafa725d35
DARIO SEGURA <darioysegura1@gmail.com>
2025-05-05 16:19:56 +0200

    Initial Commit

---
+++ src/main/java/AISS/BitbucketMiner/BitbucketMinerApplication.java
@@ -0,0 +1,13 @@
+package AISS.BitbucketMiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitbucketMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitbucketMinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AISS/BitbucketMiner/BitbucketMinerApplicationTests.java
@@ -0,0 +1,13 @@
+package AISS.BitbucketMiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitbucketMinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
